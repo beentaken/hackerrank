ab5e62a39693474c940ba6bd116bd2c3d64b2be5
Mark Nuttall-Smith <mark@gridarrow.com>
2018-12-17 18:14:51 +0100

    sorting tests

---
+++ java/src/hackerrank/sorting/ActivityNotifications.java
@@ -0,0 +1,160 @@
+package hackerrank.sorting;
+
+import java.awt.Toolkit;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Scanner;
+import java.util.SortedSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+public class ActivityNotifications {
+
+
+    // Complete the activityNotifications function below.
+//    static int activityNotifications(int[] expenditure, int d) {
+//
+//        int notifications = 0;
+//        for (int i = d; i < expenditure.length; i++) {
+//            int[] window = Arrays.copyOfRange(expenditure, i - d, i);
+//            Arrays.sort(window);
+//            double median = getMedian(window);
+//            if (expenditure[i] >= median * 2) {
+//                notifications++;
+//            }
+//        }
+//        return notifications;
+//    }
+    static int activityNotifications(int[] expenditure, int d) {;
+
+        int notificationCount = 0;
+
+        int[] data = new int[201];
+        for (int i = 0; i < d; i++) {
+            data[expenditure[i]]++;
+        }
+
+        for (int i = d; i < expenditure.length; i++) {
+
+            double median = getMedian(d, data);
+
+            if (expenditure[i] >= 2 * median) {
+                notificationCount++;
+
+            }
+
+            data[expenditure[i]]++;
+            data[expenditure[i - d]]--;
+
+        }
+
+        return notificationCount;
+
+    }
+
+    private static double getMedian(int d, int[] data) {
+        double median = 0;
+        if (d % 2 == 0) {
+            Integer m1 = null;
+            Integer m2 = null;
+            int count = 0;
+            for (int j = 0; j < data.length; j++) {
+                count += data[j];
+                if (m1 == null && count >= d/2) {
+                    m1 = j;
+                }
+                if (m2 == null && count >= d/2 + 1) {
+                    m2 = j;
+                    break;
+                }
+            }
+            median = (m1 + m2) / 2.0;
+        } else {
+            int count = 0;
+            for (int j = 0; j < data.length; j++) {
+                count += data[j];
+                if (count > d/2) {
+                    median = j;
+                    break;
+                }
+            }
+        }
+        return median;
+    }
+
+    private static void runTestCase(String testCase, Scanner scanner, Scanner output) {
+
+        StringWriter bufferedWriter = new StringWriter();
+
+        // ----------------
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] expenditure = new int[n];
+
+        String[] expenditureItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int expenditureItem = Integer.parseInt(expenditureItems[i]);
+            expenditure[i] = expenditureItem;
+        }
+
+        int result = activityNotifications(expenditure, d);
+
+        bufferedWriter.write(String.valueOf(result));
+        // ----------------
+
+        String resultStr = bufferedWriter.toString();
+        String outputStr = output.useDelimiter("\\A").next().strip();
+
+        if (!outputStr.equals(resultStr)) {
+            System.out.println(String.format("FAIL: %s\nEXPECTED: %s\nACTUAL:   %s\n---",
+                    testCase, outputStr, resultStr));
+            Toolkit.getDefaultToolkit().beep();
+        } else {
+            System.out.println(String.format("SUCCESS: %s\nEXPECTED: %s\nACTUAL:   %s\n---",
+                    testCase, outputStr, resultStr));
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        String testCases = System.getenv("TESTCASES") + "/fraudulent-activity-notifications-testcases.zip";
+
+        Pattern pattern = Pattern.compile("input/input(\\d+).txt");
+
+        ZipFile zipFile = new ZipFile(testCases);
+        Enumeration<? extends ZipEntry> entries = zipFile.entries();
+        while (entries.hasMoreElements()) {
+            ZipEntry entry = entries.nextElement();
+            Matcher matcher = pattern.matcher(entry.getName());
+            while (matcher.find()) {
+                String testCase = matcher.group(1);
+
+                Scanner inputScanner = new Scanner(zipFile.getInputStream(entry));
+
+                ZipEntry output = zipFile.getEntry(String.format("output/output%s.txt",
+                        testCase));
+                Scanner outputScanner = new Scanner(zipFile.getInputStream(output));
+
+                runTestCase(testCase, inputScanner, outputScanner);
+
+                inputScanner.close();
+                outputScanner.close();
+            }
+
+        }
+
+    }
+
+}
