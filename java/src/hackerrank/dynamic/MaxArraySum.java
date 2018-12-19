a00508dfb253079d24c14b57210f88048968df53
Mark Nuttall-Smith <mark@gridarrow.com>
2018-12-19 09:09:59 +0100

    algorithms

---
+++ java/src/hackerrank/dynamic/MaxArraySum.java
@@ -0,0 +1,108 @@
+package hackerrank.dynamic;
+
+import java.awt.Toolkit;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+public class MaxArraySum {
+
+    static int maxSubsetSum(int[] arr, int i, int[] memo) {
+        if (i == arr.length) {
+            return 0;
+        }
+        if (i == arr.length - 1) {
+            return arr[i];
+        }
+
+        if (memo[i] == 0) {
+
+            memo[i] = Math.max(arr[i] + maxSubsetSum(arr, i + 2, memo),
+                    maxSubsetSum(arr, i + 1, memo));
+        }
+        return memo[i];
+
+
+    }
+
+    // Complete the maxSubsetSum function below.
+    static int maxSubsetSum(int[] arr) {
+        int[] memo = new int[arr.length];
+        return maxSubsetSum(arr, 0, memo);
+    }
+
+    private static void runTestCase(String testCase, Scanner scanner, Scanner output) {
+
+        StringWriter bufferedWriter = new StringWriter();
+
+        // ----------------
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int res = maxSubsetSum(arr);
+
+        bufferedWriter.write(String.valueOf(res));
+
+        // ----------------
+
+        String resultStr = bufferedWriter.toString();
+        String outputStr = output.useDelimiter("\\A").next().strip();
+
+        if (!outputStr.equals(resultStr)) {
+            System.out.println(String.format("FAIL: %s\nEXPECTED: %s\nACTUAL:   %s\n---",
+                    testCase, outputStr, resultStr));
+            Toolkit.getDefaultToolkit().beep();
+        } else {
+            System.out.println(String.format("SUCCESS: %s\nEXPECTED: %s\nACTUAL:   %s\n---",
+                    testCase, outputStr, resultStr));
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        String testCases = System.getenv("TESTCASES") + "/max-array-sum-testcases.zip";
+
+        Pattern pattern = Pattern.compile("input/input(\\d+).txt");
+
+        ZipFile zipFile = new ZipFile(testCases);
+        Enumeration<? extends ZipEntry> entries = zipFile.entries();
+        while (entries.hasMoreElements()) {
+            ZipEntry entry = entries.nextElement();
+            Matcher matcher = pattern.matcher(entry.getName());
+            while (matcher.find()) {
+                String testCase = matcher.group(1);
+
+                Scanner inputScanner = new Scanner(zipFile.getInputStream(entry));
+
+                ZipEntry output = zipFile.getEntry(String.format("output/output%s.txt",
+                        testCase));
+                Scanner outputScanner = new Scanner(zipFile.getInputStream(output));
+
+                runTestCase(testCase, inputScanner, outputScanner);
+
+                inputScanner.close();
+                outputScanner.close();
+            }
+
+        }
+
+    }
+
+}
